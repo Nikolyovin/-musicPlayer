31365cbb012f74abbf774679483beaf0c6c2617c
Nikolyovin <pulya0763@gmail.com>
2022-08-21 16:04:05 +0300

    dynamic change audio

--- src/components/Footer/AudioPlayer/AudioPlayer.jsx
+++ src/components/Footer/AudioPlayer/AudioPlayer.jsx
@@ -1,18 +1,47 @@
 import styles from "./AudioPlayer.module.css"
 import track1 from "../../../assets/tracks/1.mp3"
+import track2 from "../../../assets/tracks/1.mp3"
 import { useEffect, useRef, useState } from "react"
 import { Slider } from "@mui/material"
+import { useSelector } from "react-redux"
 
+//https://developer.chrome.com/blog/play-request-was-interrupted/
 //www.w3schools.com html reference audio/video
 const AudioPlayer = () => {
     const [isPlaying, setIsPlaying] = useState(false)
     const [duration, setDuration] = useState(0)
     const [currentTime, setCurrentTime] = useState(0)
+    // const [source, setSource] = useState(track1)
 
+    const track = useSelector((state) => state.player.activeTrack?.track) 
+    console.log('track:', track)
+
+    // const trackUrl = "https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3";
+    // const trackUrl2 ="https://cdn.simplecast.com/audio/cae7b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3";
+    // const arr = [track1, track2]
+    // const currentTrack = arr.find(item => item == track)
 
     const audioPlayer = useRef();   // reference our audio component
     const progressBar = useRef();   // reference our progress bar
     const animationRef = useRef();  // reference the animation
+
+    if (audioPlayer.current) audioPlayer.current.load()
+    
+    
+
+    //динамическое изменение песни
+    // useEffect(() => {
+    //     console.log('source1:', source)
+    //     setSource(currentTrack?.track)
+    //     console.log('source2:', source)
+    //         if(audioPlayer.current){
+    //             console.log('source3:', source)
+    //             audioPlayer.current.pause();
+    //             audioPlayer.current.load();
+    //             audioPlayer.current.play();
+    //         }
+        
+    // }, [currentTrack])
 
 
     const onLoadedMetadata = () => {
@@ -69,7 +98,7 @@ const AudioPlayer = () => {
     return (
             <div className={styles.audioPlayer}>
                 <input type="range" defaultValue={0} ref = {progressBar} onChange={changeRange} className={styles.progressBar}/>
-                <audio onLoadedMetadata={onLoadedMetadata} className={styles.audio} ref={audioPlayer} src={track1} type="audio/mp3" controls></audio>
+                <audio  onLoadedMetadata={onLoadedMetadata} className={styles.audio} ref={audioPlayer} src={track} type="audio/mp3" controls></audio>
                 
                 <button onClick={togglePlayPause}>{isPlaying ? "Pause" : "Play"}</button>
                 {/* current time */}
