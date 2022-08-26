f316a580669043fa23f291edaab28dfae943fa7b
Nikolyovin <pulya0763@gmail.com>
2022-08-26 22:26:18 +0300

    Updates

--- src/redux/redux-store.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from "redux"
-import playerReducer from "./playerReducer"
-
-const reducers = combineReducers({
-    player: playerReducer
-})
-
-export const store = createStore(reducers)
